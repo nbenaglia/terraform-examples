fb6830e95a9b7c58af1d27f42f6219cdd6bf22dd
Nicola Benaglia <nico.benaz@gmail.com>
2018-11-20 13:03:40 +0100

    Script to install python

--- aws/instance/instance.tf
+++ aws/instance/instance.tf
@@ -3,4 +3,5 @@ resource "aws_instance" "ansible-test" {
   ami           = "${lookup(var.AMIS, var.AWS_REGION)}"
   instance_type = "t2.micro"
   key_name      = "${aws_key_pair.mykeypair.key_name}"
+  user_data     = "${data.template_cloudinit_config.cloudinit-instance.rendered}"
 }
